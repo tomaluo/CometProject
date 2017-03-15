53b3f28df5a4545415cbbd08d17d256b715c44c3
toma <lhlhseed@gmail.com>
2017-03-15 16:33:10 +0800

    增加一个 behavior 的测试demo

---
+++ app/src/main/java/com/mazing/com/cavasdemo/BehaviorActivity.java
@@ -0,0 +1,18 @@
+package com.mazing.com.cavasdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by user on 17/3/15.
+ */
+
+public class BehaviorActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_behavior);
+    }
+}
